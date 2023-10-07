5ab1e2eb3d198c52ea401b04986a872881d20018
Jose Luis <wildchamo@gmail.com>
2023-10-07 00:06:01 -0500

    corección estilos boton

--- src/app/components/menuInferior.js
+++ src/app/components/menuInferior.js
@@ -8,12 +8,14 @@ import call from "../../../public/call.png"
 export default function MenuInferior(){
     return(
         <footer className="bg-secundary mr-8 ml-8 h-20 rounded-2xl fixed inset-x-0 bottom-2">
+        
 
-
+        
         <div className="circulo rounded-full flex flex-col items-center justify-center">
-            <div className="bg-yellowCall w-24 h-24 rounded-full flex items-center justify-center">
-            <Image src={call} width={50} height={50}></Image>
-            </div>        
+            
+            <a href="tel:3142758675" className="bg-yellowCall w-20 h-20 rounded-full flex items-center justify-center">
+            <Image src={call} width={40} height={40}></Image>
+            </a>        
         </div>
         </footer>
     )
