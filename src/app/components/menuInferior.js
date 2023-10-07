d72b9fd24d1c4d092f8c40ef86fbc9e4bd0c2021
Jose Luis <wildchamo@gmail.com>
2023-10-07 00:20:00 -0500

    footer listo

--- src/app/components/menuInferior.js
+++ src/app/components/menuInferior.js
@@ -7,11 +7,11 @@ import call from "../../../public/call.png";
 
 export default function MenuInferior() {
   return (
-    <footer className="bg-secundary flex gap-8 mr-12 items-center pl-4 ml-8 h-20 rounded-2xl fixed inset-x-0 bottom-2">
-        <NavAction src={homePage} />
-        <NavAction src={doorbell} />
-        <NavAction src={user} />
-
+    <footer className="bg-secundary flex gap-4 mr-12 items-center pl-4 ml-8 h-20 rounded-2xl fixed inset-x-0 bottom-2">
+        <NavAction src={homePage} isActive={true} />
+        <NavAction src={doorbell} isActive={false}  />
+        <NavAction src={user} isActive={false}  />
+ 
       <div className="circulo rounded-full flex flex-col items-center justify-center">
         <a
           href="tel:3142758675"
--- src/app/components/navAction.js
+++ src/app/components/navAction.js
@@ -1,9 +1,16 @@
 import Image from "next/image";
 
-export default function NavAction({src}){
+export default function NavAction({src,isActive}){
     return(
+        <>
+        <div
+            className={` ${
+                isActive ? "bg-terciary w-20 h-20 rounded-full flex items-center justify-center" : ""
+              }`}>
         <Image src={src} width={50} height={50}>    
         </Image>
+        </div>
+        </>
 
     )
 }
