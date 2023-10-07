f3c3d184dab265f13a2f41ea95c73e431f460aa3
Jose Luis <wildchamo@gmail.com>
2023-10-06 21:11:02 -0500

    wscreen para el gradient

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import LoginBar from "./components/LoginBar";
 
 export default function Login() {
   return (
-    <main className="w-100 h-100 bg-gradient-to-t from-gradientgreen to-white">
+    <main className="w-screen h-screen bg-gradient-to-t from-gradientgreen to-white">
       <LoginBar />
     </main>
   );
