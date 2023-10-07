92e535d0227e2fa895eb02289f7cc15fb25a4ac6
Jose Luis <wildchamo@gmail.com>
2023-10-06 23:55:50 -0500

    call ready

--- src/app/components/menuInferior.js
+++ src/app/components/menuInferior.js
@@ -1,4 +1,8 @@
-import HomePage from "../../../public/homepage.png"
+import Image from "next/image"
+import homePage from "../../../public/homepage.png"
+import doorbell from "../../../public/doorbell.png"
+import user from "../../../public/user.png"
+import call from "../../../public/call.png"
 
 
 export default function MenuInferior(){
@@ -7,7 +11,8 @@ export default function MenuInferior(){
 
 
         <div className="circulo rounded-full flex flex-col items-center justify-center">
-            <div className="bg-yellowCall w-24 h-24 rounded-full">
+            <div className="bg-yellowCall w-24 h-24 rounded-full flex items-center justify-center">
+            <Image src={call} width={50} height={50}></Image>
             </div>        
         </div>
         </footer>
