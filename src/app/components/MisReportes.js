5d3fd81aa26ee153f4a5c4ab2a0bbc6750debf2e
Jose Luis <wildchamo@gmail.com>
2023-10-06 20:49:51 -0500

    gap listo

--- src/app/components/MisReportes.js
+++ src/app/components/MisReportes.js
@@ -1,10 +1,16 @@
 "use client";
+import ActionSection from "./ActionSection";
+import generarReporte from "../../../public/generarReporte.png";
+import historial from "../../../public/historial.png";
 
-
-export default function MisReportes(){
-    return(
-        <section className="">
-            <h2>MIS REPORTES</h2>
-        </section>
-    )
+export default function MisReportes() {
+  return (
+    <section className="">
+      <h2>MIS REPORTES</h2>
+      <div className="flex justify-start gap-4 bg-primary h-32 p-6 rounded-2xl text-sm">
+        <ActionSection src={generarReporte} text="Generar reporte"></ActionSection>
+        <ActionSection src={historial} text="Historial"></ActionSection>
+      </div>
+    </section>
+  );
 }
