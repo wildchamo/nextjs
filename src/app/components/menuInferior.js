811d5f67c3891841ebb6cb8e649a383c26287e3c
Jose Luis <wildchamo@gmail.com>
2023-10-06 23:30:34 -0500

    corrección de estilos

--- src/app/components/menuInferior.js
+++ src/app/components/menuInferior.js
@@ -1,7 +1,6 @@
 export default function MenuInferior(){
     return(
         <nav className="bg-secundary mr-8 ml-8 h-20 rounded-2xl relative ">
-            holaa
         <div className="circulo">
 
         </div>
