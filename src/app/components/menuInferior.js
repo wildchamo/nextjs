b40bb8237e50607c455df5b7c9085d474df08130
Jose Luis <wildchamo@gmail.com>
2023-10-06 23:41:33 -0500

    insent y bottom al footer

--- src/app/components/menuInferior.js
+++ src/app/components/menuInferior.js
@@ -1,6 +1,11 @@
+import HomePage from "../../../public/homepage.png"
+
+
 export default function MenuInferior(){
     return(
-        <nav className="bg-secundary mr-8 ml-8 h-20 rounded-2xl relative ">
+        <nav className="bg-secundary mr-8 ml-8 h-20 rounded-2xl fixed inset-x-0 bottom-2">
+
+
         <div className="circulo">
 
         </div>
