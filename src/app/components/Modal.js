e80eab410465e7416233c5efcf62980a5643936d
Jose Luis <wildchamo@gmail.com>
2023-10-09 10:52:45 -0500

    opacidad al modal

--- src/app/components/Modal.js
+++ src/app/components/Modal.js
@@ -1,6 +1,6 @@
 export default function Modal({ children }) {
   return (
-    <div className="fixed inset-0 z-10 overflow-y-auto">
+    <div className="fixed inset-0 z-10 overflow-y-auto bg-black bg-opacity-25	">
       <div className="flex items-center justify-center min-h-screen">
         <div className="bg-white rounded-lg shadow-lg p-6 w-80">{children}</div>
       </div>
