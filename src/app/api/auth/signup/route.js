8e23c5956393a948343805871a126e89f56d36a7
Jose Luis <wildchamo@gmail.com>
2023-10-13 18:52:11 -0500

    instalación de bcrypt

--- src/app/api/auth/signup/route.js
+++ src/app/api/auth/signup/route.js
@@ -1,12 +1,12 @@
 import { connectDB } from "@/app/utils/mongoose";
 import User from "@/models/users";
 import { NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
 
 export async function POST(request) {
   try {
     await connectDB();
     const { nombre, password, identificacion, ciudad } = await request.json();
-
 
     const userfound = await User.findOne({ identificacion });
 
@@ -20,18 +20,19 @@ export async function POST(request) {
         }
       );
 
+    const hashedPassword = await bcrypt.hash(identificacion, 12);
+
     const user = new User({
       nombre,
-      password,
       identificacion,
+      password: hashedPassword,
       ciudad,
     });
 
     const savedUser = await user.save();
 
     console.log(savedUser);
- 
- 
+
     return NextResponse.json(savedUser);
   } catch (error) {
     console.log(error);
