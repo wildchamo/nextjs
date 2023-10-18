7dd0b30becf41e6bb95fa20d947923febbf4dcb0
Jose Luis <wildchamo@gmail.com>
2023-10-17 22:46:53 -0500

    botón para generar reportes desplegando el modal

--- src/app/components/ActionSection.js
+++ src/app/components/ActionSection.js
@@ -1,12 +1,10 @@
 import Image from "next/image";
 
-export default function ActionSection({src,text}) {
+export default function ActionSection({src,text,onClick}) {
   return (
-    <section className="">
-      <div className="flex flex-col justify-center items-center bg-secundary w-20 h-20 p-1 text-white rounded-2xl text-sm">
+      <div onClick={onClick} className="flex flex-col justify-center items-center bg-secundary w-20 h-20 p-1 text-white rounded-2xl text-sm">
         <Image alt={text} src={src} width={30} height={30}></Image>
         <p className="text-center text-sm">{text}</p>
       </div>
-    </section>
   );
 }
--- src/app/components/MisReportes.js
+++ src/app/components/MisReportes.js
@@ -1,21 +1,45 @@
 "use client";
 import ActionSection from "./ActionSection";
+import { useState } from "react";
+import Modal from "./Modal";
 import generarReporte from "../../../public/generarReporte.png";
-import historial from "../../../public/historial.png";
 
 export default function MisReportes() {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleModalClick = () => {
+    console.log("hola");
+    setShowModal(true);
+  };
   return (
     <section className="pt-6">
       <h2>MIS REPORTES</h2>
       <div className="flex justify-start gap-4 bg-primary h-32 p-6 rounded-2xl text-sm">
-        <ActionSection src={generarReporte} text="Generar reporte"></ActionSection>
-
-
-
-        {/* <ActionSection src={historial} text="Historial"></ActionSection> */}
+        <ActionSection
+          src={generarReporte}
+          text="Generar reporte"
+          onClick={handleModalClick}
+        />
       </div>
+
+      {showModal && <ModalTipo onClose={() => setShowModal(false)} />}
     </section>
   );
 }
 
+function ModalTipo({ onClose }) {
+  return (
+    <Modal className="">
+      <h2 className="text-lg font-bold mb-4">Hay lesionados implicados?</h2>
 
+      <div className="flex justify-end">
+        <button
+          className="bg-secundary text-white px-4 py-2 rounded-lg"
+          onClick={onClose}
+        >
+          Regresar
+        </button>
+      </div>
+    </Modal>
+  );
+}
