3875747b6ad27635cf0b09736b4036be05c5ab00
Jose Luis <wildchamo@gmail.com>
2023-10-20 00:29:37 -0500

    solución al menú inferior

--- src/app/home/layout.js
+++ src/app/home/layout.js
@@ -4,7 +4,7 @@ export default function LayoutHome({children}){
     return(
         <div className='flex flex-col justify-between h-screen'>
             {children}
-            {/* <MenuInferior/> */}
+            <MenuInferior/>
         </div>
     )
 }
