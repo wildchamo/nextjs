ef2dace5a8fdf373832c9fe0b7c9a6c6dd65d1d0
Jose Luis <wildchamo@gmail.com>
2023-12-03 22:32:21 -0500

    update paddings to make a better ui

--- src/app/home/reportar/page.js
+++ src/app/home/reportar/page.js
@@ -3,7 +3,7 @@ import FormMandarEmail from "../../components/FormMandarEmail";
 
 const ReportarCaso = () => {
   return (
-    <section className="mr-8 ml-8 mt-8">
+    <section className="mr-8 ml-8 mt-8 pb-52">
       <FormMandarEmail />
     </section>
   );
