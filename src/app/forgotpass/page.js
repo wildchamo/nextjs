cba8c4714b8119c5d718543e98dcadfa682379a5
Jose Luis <wildchamo@gmail.com>
2023-12-17 11:48:14 -0500

    ssr component ready for fotogotpasswordpage

--- src/app/forgotpass/page.js
+++ src/app/forgotpass/page.js
@@ -1,17 +1,20 @@
 import { ForgotPasswordForm } from "../components/forgotPassword";
 import Image from "next/image";
 import logo from "../../../public/logomayaluna.jpg";
+import Link from "next/link";
 
 export default function ForgotPasswordPage() {
   return (
     <section className="w-screen h-screen bg-gradient-to-t from-gradientGreen to-white">
-      <Image
-        className="mx-auto mb-14 mt-6"
-        src={logo}
-        width={150}
-        height={150}
-        alt="logoMayaluna"
-      />
+      <Link href="/login">
+        <Image
+          className="mx-auto mb-14 mt-6"
+          src={logo}
+          width={150}
+          height={150}
+          alt="logoMayaluna"
+        />
+      </Link>
 
       <ForgotPasswordForm />
     </section>
