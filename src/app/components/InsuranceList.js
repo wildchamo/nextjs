c4416d92110f31be043ba9a8f5e58dc714ff1595
Jose Luis <wildchamo@gmail.com>
2023-12-16 21:19:28 -0500

    update behavior on insurance list

--- src/app/components/InsuranceList.js
+++ src/app/components/InsuranceList.js
@@ -15,40 +15,44 @@ const InsuranceList = () => {
   };
 
   return (
-    <div className="grid grid-cols-[1fr,1fr,auto] gap-4 bg-primary py-4 px-4 rounded-2xl">
-      <div className="font-bold">Seguro</div>
-      <div className="font-bold">Vencimiento</div>
-      <div className="font-bold">Ver más</div>
-
-      {seguros.map((seguro) => (
-        <React.Fragment key={seguro.id}>
-          <div>{seguro.seguro}</div>
-          <div>{seguro.vencimiento}</div>
-          <div className="flex justify-center items-center">
-            <div
-              className="flex justify-center items-center bg-secondary rounded-full h-5 w-5"
-              onClick={() => seeIndiviudalSeguro(seguro.id)}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="14"
-                height="14"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  fill="none"
-                  stroke="#ffffff"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M3 10a7 7 0 1 0 14 0a7 7 0 1 0-14 0m18 11l-6-6"
-                />
-              </svg>
+    <section className="bg-primary py-4 px-4 rounded-2xl">
+      <div className="grid grid-cols-[2fr,2fr,auto] ">
+        <div className="font-bold">Seguro</div>
+        <div className="font-bold">Vencimiento</div>
+        <div className="font-bold">Ver más</div>
+      </div>
+      <div>
+        {seguros.map((seguro) => (
+          <div
+            className="grid grid-cols-[2fr,2fr,1fr] pt-3 text-left"
+            key={seguro.id}
+            onClick={() => seeIndiviudalSeguro(seguro.id)}
+          >
+            <div>{seguro.seguro}</div>
+            <div>{seguro.vencimiento}</div>
+            <div className="flex justify-center items-center">
+              <div className="flex justify-center items-center bg-secondary rounded-full h-5 w-5">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="14"
+                  height="14"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill="none"
+                    stroke="#ffffff"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M3 10a7 7 0 1 0 14 0a7 7 0 1 0-14 0m18 11l-6-6"
+                  />
+                </svg>
+              </div>
             </div>
           </div>
-        </React.Fragment>
-      ))}
-    </div>
+        ))}
+      </div>
+    </section>
   );
 };
 
