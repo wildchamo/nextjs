1436acd719ed12ca78bc0094adbe68a789a8f815
Jose Luis <wildchamo@gmail.com>
2023-12-25 16:33:38 -0500

    reading insurances almost ready, missing docs

--- src/app/home/seguros/[id]/page.js
+++ src/app/home/seguros/[id]/page.js
@@ -14,34 +14,40 @@ const SeguroIndividual = ({ params }) => {
   }, []);
 
   return (
-    <div className="bg-primary py-4 px-4 rounded-2xl">
-      <h2 className="font-bold pb-4 text-xl">{seguro.nombrePoliza}</h2>
-      <section className="grid grid-cols-2">
-        <p className="text-left">Nombre póliza</p>
-        <p className="text-right">{seguro.nombrePoliza}</p>
-      </section>{" "}
-      <section className="grid grid-cols-2">
-        <p className="text-left">Tipo póliza</p>
-        <p className="text-right">{seguro.tipoPoliza}</p>
-      </section>{" "}
-      <section className="grid grid-cols-2">
-        <p className="text-left">Compañia aseguradora</p>
-        <p className="text-right">{seguro.companiaAseguradora}</p>
-      </section>{" "}
-      <section className="grid grid-cols-2">
-        <p className="text-left">Fecha vencimiento</p>
-        <p className="text-right">
-          {seguro.fechaVencimiento && !isNaN(new Date(seguro.fechaVencimiento))
-            ? new Date(seguro.fechaVencimiento).toISOString().split("T")[0]
-            : "Fecha no disponible"}
-        </p>
-      </section>
-      <div className="text-end mt-4">
-        <button className="shadow bg-secondary focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">
-          Ver póliza
-        </button>
+    <>
+      <div className="bg-primary py-4 px-4 rounded-2xl">
+        <h2 className="font-bold pb-4 text-xl">Información general</h2>
+        <section className="grid grid-cols-2">
+          <p className="text-left">Nombre póliza</p>
+          <p className="text-right">{seguro.nombrePoliza}</p>
+        </section>{" "}
+        <section className="grid grid-cols-2">
+          <p className="text-left">Tipo póliza</p>
+          <p className="text-right">{seguro.tipoPoliza}</p>
+        </section>{" "}
+        <section className="grid grid-cols-2">
+          <p className="text-left">Compañia aseguradora</p>
+          <p className="text-right">{seguro.companiaAseguradora}</p>
+        </section>{" "}
+        <section className="grid grid-cols-2">
+          <p className="text-left">Fecha vencimiento</p>
+          <p className="text-right">
+            {seguro.fechaVencimiento &&
+            !isNaN(new Date(seguro.fechaVencimiento))
+              ? new Date(seguro.fechaVencimiento).toISOString().split("T")[0]
+              : "Fecha no disponible"}
+          </p>
+        </section>
       </div>
-    </div>
+
+      <div className="bg-primary py-4 px-4 mt-4 rounded-2xl">
+        <h2 className="font-bold pb-4 text-xl">Documentos</h2>
+        <section className="grid grid-cols-2">
+          <p className="text-left">Documento</p>
+          <p className="text-right">{seguro.nombrePoliza}</p>
+        </section>{" "}
+      </div>
+    </>
   );
 };
 
