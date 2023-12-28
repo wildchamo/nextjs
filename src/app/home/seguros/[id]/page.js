3ad2fd117266e9ca704a4c214a601b66826013be
Jose Luis <wildchamo@gmail.com>
2023-12-27 20:51:23 -0500

    update insurances companies ready

--- src/app/home/seguros/[id]/page.js
+++ src/app/home/seguros/[id]/page.js
@@ -1,17 +1,28 @@
 "use client";
 import useUserStore from "../../../stores/userStore";
 import { useState, useEffect } from "react";
+import { companiasSeguros } from "../../../utils/insuranceCompaniesList";
 
 const SeguroIndividual = ({ params }) => {
   const { seguros } = useUserStore((state) => state);
 
   const [seguro, setSeguro] = useState({
     seguro: "",
+    asistencia: "",
   });
 
   useEffect(() => {
-    setSeguro(seguros.find((seguro) => seguro._id === params.id));
-  }, []);
+    const seguroEncontrado = seguros.find((seguro) => seguro._id === params.id);
+    if (seguroEncontrado) {
+      const companiaSeguro = companiasSeguros.find(
+        (compania) => compania.nombre === seguroEncontrado.companiaAseguradora
+      );
+      setSeguro({
+        ...seguroEncontrado,
+        asistencia: companiaSeguro ? companiaSeguro.asistencia : undefined,
+      });
+    }
+  }, [seguros, params.id]);
 
   return (
     <>
@@ -28,6 +39,12 @@ const SeguroIndividual = ({ params }) => {
         <section className="grid grid-cols-2">
           <p className="text-left">Compañia aseguradora</p>
           <p className="text-right">{seguro.companiaAseguradora}</p>
+        </section>{" "}
+        <section className="grid grid-cols-2">
+          <p className="text-left">Número asistencia Compañia</p>
+          <a className="text-right" href={"tel:" + seguro.asistencia}>
+            {seguro.asistencia}
+          </a>
         </section>{" "}
         <section className="grid grid-cols-2">
           <p className="text-left">Fecha vencimiento</p>
