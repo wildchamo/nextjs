ac593f671c0d41fe04d8bfb39882a8b60e927abb
Jose Luis <wildchamo@gmail.com>
2023-12-30 17:31:12 -0500

    update responsinve deisgn on user panel

--- src/app/components/shared/menuInferior.js
+++ src/app/components/shared/menuInferior.js
@@ -4,27 +4,27 @@ import Image from "next/image";
 import call from "../../../../public/call.png";
 
 export default function MenuInferior() {
-
-
   return (
-    <footer className="bg-secondary flex gap-1 mr-12 items-center pl-2 ml-8 h-20 rounded-2xl fixed inset-x-0 bottom-2">
-      <NavAction href="/home"></NavAction>
-      <NavAction href="/home/user"></NavAction>
-      <LogoutButton/>
+    <div className="flex justify-center h-20 fixed inset-x-0 bottom-2">
+      <footer className="bg-secondary flex gap-1 sm:gap-2 items-center pl-2 max-w-screen-sm w-11/12 h-20 rounded-2xl">
+        <NavAction href="/home"></NavAction>
+        <NavAction href="/home/user"></NavAction>
+        <LogoutButton />
 
-      <div className="circulo rounded-full flex flex-col items-center justify-center">
-        <a
-          href="tel:123"
-          className="bg-yellowCall w-20 h-20 rounded-full flex items-center justify-center"
-        >
-          <Image
-            alt="Llamada emergencia"
-            src={call}
-            width={40}
-            height={40}
-          ></Image>
-        </a>
-      </div>
-    </footer>
+        <div className="circulo rounded-full flex flex-col items-center justify-center">
+          <a
+            href="tel:123"
+            className="bg-yellowCall w-20 h-20 rounded-full flex items-center justify-center"
+          >
+            <Image
+              alt="Llamada emergencia"
+              src={call}
+              width={40}
+              height={40}
+            ></Image>
+          </a>
+        </div>
+      </footer>
+    </div>
   );
 }
--- src/app/home/layout.js
+++ src/app/home/layout.js
@@ -3,10 +3,12 @@ import MenuSuperior from "../components/shared/menuSuperior";
 
 export default function LayoutHome({ children }) {
   return (
-    <div className="h-screen">
-      <MenuSuperior />
-      <section className="mx-8 my-4">{children}</section>
-      <MenuInferior />
+    <div className="flex justify-center h-screen">
+      <div className="max-w-screen-sm w-full">
+        <MenuSuperior />
+        <section className="mx-8 my-4">{children}</section>
+        <MenuInferior />
+      </div>
     </div>
   );
 }
