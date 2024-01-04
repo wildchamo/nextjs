09b7f1c5be5350a0543d9e7694bd0dddf845a354
Jose Luis <wildchamo@gmail.com>
2024-01-04 01:56:00 -0500

    update behavior on mis seguros section, dont use use router

--- src/app/components/home/MisSeguros.js
+++ src/app/components/home/MisSeguros.js
@@ -2,24 +2,15 @@
 import ActionSection from "./ActionSection";
 import useUserStore from "@/app/stores/userStore";
 import InsuranceButton from "./InsuranceButton";
-import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import Link from "next/link";
 
 export default function MisSeguros() {
-  const router = useRouter();
   const { seguros, getSeguros, _id } = useUserStore((state) => state);
 
   useEffect(() => {
     getSeguros(_id);
   }, []);
-
-  const seeSeguros = () => {
-    router.push("/home/seguros");
-  };
-
-  const seeIndiviudalSeguro = (id) => {
-    router.push(`/home/seguros/${id}`);
-  };
 
   return (
     <section className="pt-6">
@@ -29,18 +20,14 @@ export default function MisSeguros() {
         {seguros.length > 0 ? (
           <>
             {seguros.slice(0, 2).map((seguro) => (
-              <InsuranceButton
-                key={seguro._id}
-                text={seguro.tipoPoliza}
-                onClick={() => seeIndiviudalSeguro(seguro._id)}
-              />
+              <Link href={`/home/seguros/${seguro._id}`}>
+                <InsuranceButton key={seguro._id} text={seguro.tipoPoliza} />
+              </Link>
             ))}
 
-            <ActionSection
-              src="todos"
-              onClick={seeSeguros}
-              text="Ver todos"
-            ></ActionSection>
+            <Link href="/home/seguros">
+              <ActionSection src="todos" text="Ver todos"></ActionSection>
+            </Link>
           </>
         ) : (
           <p className="text-md">
